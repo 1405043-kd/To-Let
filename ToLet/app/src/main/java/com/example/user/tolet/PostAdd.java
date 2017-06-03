2dd1f10af3ff539956539879b2738a33da3a40ac
1405043-kd <1405043-kd@ugrad.cse.buet.ac.bd>
2017-06-03 12:33:17 +0600

    image add button added

--- ToLet/app/src/main/java/com/example/user/tolet/PostAdd.java
+++ ToLet/app/src/main/java/com/example/user/tolet/PostAdd.java
@@ -1,18 +1,24 @@
 package com.example.user.tolet;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import static com.example.user.tolet.R.layout.activity_main;
 
 public class PostAdd extends AppCompatActivity {
+    public static final int RESULT_LOAD_IMAGE=1;
     public static final String EXTRA_MESSAGE = "com.example.user.tolet";
     EditText editText1;
     EditText editText2;
@@ -24,6 +30,9 @@ public class PostAdd extends AppCompatActivity {
     Button imb;
     Switch aSwitch;
     ImageButton imageButton;
+    ImageView imageView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,8 +46,29 @@ public class PostAdd extends AppCompatActivity {
         textView=(TextView) findViewById(R.id.currState);
         imb=(Button)findViewById(R.id.postButton);
         aSwitch=(Switch) findViewById(R.id.switcha);
+        imageButton=(ImageButton) findViewById(R.id.imageButtonPic);
+        imageView=(ImageView)findViewById(R.id.imageView);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent =new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent,RESULT_LOAD_IMAGE);
+            }
+        });
 
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==RESULT_LOAD_IMAGE && resultCode==RESULT_OK && data!=null){
+            Uri selectedImage=data.getData();
+            imageView.setImageURI(selectedImage);
+     //       Bitmap bitmap=(BitmapDrawable) imageView.getDrawable().getBitMap();
+
+        }
+    }
+
+
     public void sendMessage(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         EditText editText = (EditText) findViewById(R.id.houseNO);
